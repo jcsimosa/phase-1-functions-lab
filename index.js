303fd956783c23439e3fe59a1af056d15de97460
Jose Simosa Nunez <josecarlossimosa@gmail.com>
2022-06-29 11:51:43 -0500

    commit tree 717039a622abd4452186453bcf8aba19b363093a

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(blocks){
+    if (blocks > 42) {
+     return blocks - 42
+    }
+    else {
+        return 42 - blocks
+    }
+}
+function distanceFromHqInFeet(number){
+    let numberOfBlocks = distanceFromHqInBlocks(number)
+    return numberOfBlocks * 264
+}
+function distanceTravelledInFeet(start,destination){
+    let store = distanceFromHqInBlocks(start)
+    let steps = distanceFromHqInFeet(destination)
+    return distanceFromHqInBlocks * distanceFromHqInFeet
+}
